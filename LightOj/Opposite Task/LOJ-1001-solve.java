28adef5815fd5be281c8b1ba7fd344346c1d45e2
ratul.sikder <ratul@easital.com>
2024-01-01 10:52:25 +0600

    P Solved

---
+++ LightOj/Opposite Task/LOJ-1001-solve.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+class Solution {
+
+
+    public static void main(String[] args) throws Exception {
+      Scanner scanner = new Scanner(System.in);
+      int T = scanner.nextInt(), n;
+      for (int i = 1; i <= T; i++) {
+        n = scanner.nextInt();
+        if (n > 10) {
+          System.out.printf("10 %d\n", n - 10);
+        } else {
+          System.out.printf("0 %d\n", n);
+        }
+      }
+    }
+}
+
